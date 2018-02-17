4f7beb65be20b546da2c093bd9573873b2d846e3
Vitalii Maksimov <g.vitaly.maximov@gmail.com>
2018-02-17 14:21:54 +0300

    Draw a sprite

--- public/js/main.js
+++ public/js/main.js
@@ -8,9 +8,36 @@ function loadImage(url) {
     });
 }
 
+class SpriteSheet
+{
+    constructor(image, spriteWidth, spriteHeight)
+    {
+        this.image = image;
+        this.spriteWidth = spriteWidth;
+        this.spriteHeight = spriteHeight;
+        this.tiles = new Map();
+    }
+
+    define(name, spriteX, spriteY)
+    {
+        this.tiles.set(name, { x: spriteX, y: spriteY });
+    }
+
+    draw(name, context, x, y)
+    {
+        const sprite = this.tiles.get(name);
+        context.drawImage(
+            this.image, 
+            sprite.x, sprite.y, this.spriteWidth, this.spriteHeight, 
+            x, y, this.spriteWidth, this.spriteHeight);
+    }
+}
+
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-context.fillRect(0, 0, 50, 50);
-
-loadImage("/img/tiles.png").then(image => context.drawImage(image, 0, 0));
+loadImage("/img/tiles.png").then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define("ground", 0, 0);
+    sprites.draw("ground", context, 145, 245);
+});
