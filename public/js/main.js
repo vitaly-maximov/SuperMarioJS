63d9f0ae0dcea27630d61149c4411b58e12177dc
Vitalii Maksimov <g.vitaly.maximov@gmail.com>
2018-02-17 20:08:21 +0300

    Mario simple animation

--- public/js/main.js
+++ public/js/main.js
@@ -20,6 +20,35 @@ Promise
         loadMarioSprites(), 
         loadLevel("1-1")])
     .then(([sprites, mario, level]) => {
-        level.backgrounds.forEach(background => drawBackground(background, context, sprites));
-        mario.draw("idle", context, 50, 50);
+        const backgroundBuffer = document.createElement("canvas");
+        backgroundBuffer.width = 400;
+        backgroundBuffer.height = 240;
+
+        level.backgrounds.forEach(background => drawBackground(background, backgroundBuffer.getContext("2d"), sprites));
+
+        const pos = { x: 50, y: 50 };
+        let incX = 2;
+        let incY = 2;
+
+        function update() {
+            context.drawImage(backgroundBuffer, 0, 0);
+
+            mario.draw("idle", context, pos.x, pos.y);
+
+            if (pos.x + incX < 0 || pos.x + incX > 386)
+            {
+                incX *= -1;
+            }
+            if (pos.y + incY < 0 || pos.y + incY > 176)
+            {
+                incY *= -1;
+            }
+
+            pos.x += incX;
+            pos.y += incY;
+
+            requestAnimationFrame(update);
+        }
+
+        update();        
     });
